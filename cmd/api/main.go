8690e6fbf5248f070fd9a916ef15b514e252464b
Oleh <zaaqq135@gmail.com>
2025-04-01 02:11:17 +0300

    updated handlers

---
+++ cmd/api/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+type config struct {
+	port int
+}
+
+type application struct {
+	config   config
+	infoLog  *log.Logger
+	errorLog *log.Logger
+}
+
+func main() {
+	var cfg config
+	cfg.port = 8089
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := application{
+		config:   cfg,
+		infoLog:  infoLog,
+		errorLog: errorLog,
+	}
+
+	err := app.serve()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (app *application) serve() error {
+	app.infoLog.Println("API listening on port", app.config.port)
+	return http.ListenAndServe(fmt.Sprintf(":%d", app.config.port), app.routes())
+}
